3fe659f76021cdc766070858f659740c3c9c663c
kwiersma <kwiersma@gmail.com>
2014-01-07 20:25:54 -0600

    Refactored the model to include a many to many reference to Author from Book. Added migrations.

--- stacks/library/models.py
+++ stacks/library/models.py
@@ -1,21 +1,29 @@
 from django.db import models
 
+class Author(models.Model):
+    uuid = models.CharField(max_length=36, default=None, null=False, db_index=True)
+    lastname = models.CharField(max_length=50)
+    firstname = models.CharField(max_length=50)
+
 
 class Book(models.Model):
-	uuid = models.CharField(max_length=36, default=None, null=False, db_index=True)
-	title = models.CharField(max_length=200, null=False)
-	author_lastname = models.CharField(max_length=50)
-	author_firstname = models.CharField(max_length=50)
-	description = models.TextField(null=True)
-	publish_date = models.DateField(null=True)
-	isbn10 = models.CharField(max_length=10, null=True)
-	isbn13 = models.CharField(max_length=13, null=True)
-	publisher = models.CharField(max_length=100, null=True)
+    uuid = models.CharField(max_length=36, default=None, null=False, db_index=True)
+    title = models.CharField(max_length=200, null=False)
 
-	def __unicode__(self):
-		return self.title
+    authors = models.ManyToManyField(Author)
 
-	class Meta:
-		verbose_name = 'Book'
-		verbose_name_plural = 'Books'
+    description = models.TextField(null=True)
+    publish_date = models.DateField(null=True)
+    isbn10 = models.CharField(max_length=10, null=True)
+    isbn13 = models.CharField(max_length=13, null=True)
+    publisher = models.CharField(max_length=100, null=True)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+
+
 
