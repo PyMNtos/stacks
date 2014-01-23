1867750de45ae8d9f0f217defff3b0a6e8f3c1ab
Raymond Finzel <prometheoid@gmail.com>
2014-01-22 22:03:20 -0600

    added index template and routing. Fixed bootstrap.

--- stacks/stacks/urls.py
+++ stacks/stacks/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^$', "library.views.showIndex"),
     url(r'books/', "library.views.showBooks"),
 
 
