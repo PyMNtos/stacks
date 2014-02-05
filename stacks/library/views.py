148128009fa5e3861fec36ca9515a8cca86353c9
peterfarrell <pfarrell@greatbiztools.com>
2014-02-05 17:50:19 -0600

    modified:   stacks/library/templates/base.html 	modified:   stacks/library/templates/books.html 	new file:   stacks/library/templates/index.html 	modified:   stacks/library/views.py 	modified:   stacks/stacks/settings.py 	renamed:    stacks/static/css/bootstrap-theme.css -> stacks/stacks/static/css/bootstrap-theme.css 	renamed:    stacks/static/css/bootstrap-theme.min.css -> stacks/stacks/static/css/bootstrap-theme.min.css 	renamed:    stacks/static/css/bootstrap.css -> stacks/stacks/static/css/bootstrap.css 	renamed:    stacks/static/css/bootstrap.min.css -> stacks/stacks/static/css/bootstrap.min.css 	renamed:    stacks/static/fonts/glyphicons-halflings-regular.eot -> stacks/stacks/static/fonts/glyphicons-halflings-regular.eot 	renamed:    stacks/static/fonts/glyphicons-halflings-regular.svg -> stacks/stacks/static/fonts/glyphicons-halflings-regular.svg 	renamed:    stacks/static/fonts/glyphicons-halflings-regular.ttf -> stacks/stacks/static/fonts/glyphicons-halflings-regular.ttf 	renamed:    stacks/static/fonts/glyphicons-halflings-regular.woff -> stacks/stacks/static/fonts/glyphicons-halflings-regular.woff 	renamed:    stacks/static/js/bootstrap.js -> stacks/stacks/static/js/bootstrap.js 	renamed:    stacks/static/js/bootstrap.min.js -> stacks/stacks/static/js/bootstrap.min.js 	modified:   stacks/stacks/urls.py

--- stacks/library/views.py
+++ stacks/library/views.py
@@ -9,3 +9,11 @@ def showBooks(request):
     return render(request, "books.html", {'books': books})
 
 # Create your views here.
+
+def showIndex(request):
+    books = Book.objects.all()
+    section = {'title': 'stacks',
+               'subtitle' : 'the library app'}
+    return render(request, 
+                  "index.html", 
+                  { 'books': books, 'section': section})
